05841f52dfd6eb4e8c8efcb83682544a53266464
Christophe Millot <cmillot2004@gmail.com>
2025-07-08 00:08:01 +0200

    Restructuration du HTML avec partiels PHP

---
+++ includes/_footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <div class="footer-content">
+        <p>&copy; 2025 Christophe MILLOT. Tous droits réservés.</p>
+        <div class="social-links">
+            <a href="https://www.linkedin.com/in/christophemillot/" target="_blank" aria-label="LinkedIn"><i class="fab fa-linkedin"></i></a>
+        </div>
+    </div>
+</footer>
+
+<script src="assets/js/script.js"></script>
+<script src="assets/js/scroll.js"></script> 
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,120 @@
+<?php include 'includes/_header.php'; ?>
+
+
+
+
+
+<section class="hero-section" id="hero-section">
+    <div class="hero-image-container">
+        <div class="hero-text">
+            <h1>Au service de vos idées !</h1>
+        </div>
+
+
+        <a href="#premier-bloc" class="scroll-down-arrow" aria-label="Faire défiler vers le contenu">
+            <i class="fas fa-chevron-down"></i>
+        </a>
+
+
+        <picture>
+            <source srcset="assets/images/hero-2560x1440-index.png" media="(min-width: 769px)">
+            <img src="assets/images/hero-2560x1440-index.png" alt="Image principale du héro, représentant un paysage ou une scène pertinente">
+        </picture>
+    </div>
+</section>
+
+
+
+
+
+<main class="main-content-wrapper" id="procedure-git">
+
+    <section class="content-section" id="premier-bloc">
+        <h1 class="h1Black">Titre Principal</h1>
+        <!--<p>
+            <code>
+            cd C:\xampp\htdocs\scratch<br>
+            sass --watch assets/scss:assets/css
+            </code>
+        </p>-->
+        <p>
+            <b>FAIRE LE :</b><i>Mon parcours</i> débute là où l'art prend forme sous les doigts : dans les ateliers de dessin,de peinture et de sculpture. C'est avec le crayon, le pinceau et la terre que j'ai apprisles principes fondamentaux de la composition, des volumes, des couleurs et des lumières.Ces années dédiées aux arts plastiques, enrichies par des expériences en modélisme,sont la pierre angulaire de ma vision créative et nourrissent chaque projet numériqueavec une compréhension intuitive de l'esthétique et de la forme.
+        </p>
+        <p class="text-center-with-margin"><a href="#" class="button">Découvrir mes influences artistiques</a></p>
+    </section>
+
+
+    <div class="deuxBlocs">
+        <section class="content-section">
+            <h2>BLOC01</h2>
+            <p>Fort de deux décennies d'expérience dans l'infographie, j'ai développé une expertise
+            pointue en Publication Assistée par Ordinateur (PAO) et en création graphique pour
+            l'impression. J'ai ensuite appliqué cette maîtrise du visuel pour concevoir des
+            éléments graphiques percutants, destinés à enrichir les sites web, sans toucher
+            au développement, en me concentrant sur l'impact esthétique et la clarté du message.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+        <section class="content-section">
+            <h2>BLOC02</h2>
+            <p>Conscient qu'une interface ne suffit pas sans une interaction fluide, je me suis
+            spécialisé en UX/UI Design. J'ai appris à penser l'expérience de l'utilisateur
+            dans sa globalité, de l'idéation à la création de parcours intuitifs. Grâce à des
+            outils comme Figma, je donne vie rapidement aux concepts, testant les usages pour
+            affiner l'ergonomie, assurant des interfaces à la fois esthétiques et fonctionnelles.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+    </div>
+
+    <div class="quatresBlocs">
+        <section class="content-section">
+            <h2>BLOC03</h2>
+            <p>Fort de deux décennies d'expérience dans l'infographie, j'ai développé une expertise
+            pointue en Publication Assistée par Ordinateur (PAO) et en création graphique pour
+            l'impression. J'ai ensuite appliqué cette maîtrise du visuel pour concevoir des
+            éléments graphiques percutants, destinés à enrichir les sites web, sans toucher
+            au développement, en me concentrant sur l'impact esthétique et la clarté du message.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+        <section class="content-section">
+            <h2>BLOC04</h2>
+            <p>Conscient qu'une interface ne suffit pas sans une interaction fluide, je me suis
+            spécialisé en UX/UI Design. J'ai appris à penser l'expérience de l'utilisateur
+            dans sa globalité, de l'idéation à la création de parcours intuitifs. Grâce à des
+            outils comme Figma, je donne vie rapidement aux concepts, testant les usages pour
+            affiner l'ergonomie, assurant des interfaces à la fois esthétiques et fonctionnelles.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+        <section class="content-section">
+            <h2>BLOC03</h2>
+            <p>Fort de deux décennies d'expérience dans l'infographie, j'ai développé une expertise
+            pointue en Publication Assistée par Ordinateur (PAO) et en création graphique pour
+            l'impression. J'ai ensuite appliqué cette maîtrise du visuel pour concevoir des
+            éléments graphiques percutants, destinés à enrichir les sites web, sans toucher
+            au développement, en me concentrant sur l'impact esthétique et la clarté du message.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+        <section class="content-section">
+            <h2>BLOC04</h2>
+            <p>Conscient qu'une interface ne suffit pas sans une interaction fluide, je me suis
+            spécialisé en UX/UI Design. J'ai appris à penser l'expérience de l'utilisateur
+            dans sa globalité, de l'idéation à la création de parcours intuitifs. Grâce à des
+            outils comme Figma, je donne vie rapidement aux concepts, testant les usages pour
+            affiner l'ergonomie, assurant des interfaces à la fois esthétiques et fonctionnelles.</p>
+            <p class="text-center-with-margin">
+                <a href="#" class="button">Lire la suite...</a>
+            </p>
+        </section>
+    </div>
+</main>
+
+<?php include 'includes/_footer.php'; ?>
